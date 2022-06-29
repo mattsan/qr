bcfec42f7ef7e305fc1555f3d9695b94bcd28692
Eiji MATSUMOTO <e.mattsan@gmail.com>
2022-06-29 23:13:26 +0900

    Squashed commit of the following:
    commit e3bf740cf0e21b05ec957f7c8bf38ca0457a92a5
    Author: Eiji MATSUMOTO <e.mattsan@gmail.com>
    Date:   Wed Jun 29 23:13:05 2022 +0900
    
        show QR code

--- lib/qr.ex
+++ lib/qr.ex
@@ -1,18 +1,43 @@
 defmodule Qr do
   @moduledoc """
-  Documentation for `Qr`.
+  QR コード
   """
+
+  import OK, only: [~>: 2]
+
+  @white IO.ANSI.light_white_background()
+  @black IO.ANSI.black_background()
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Qr.hello()
-      :world
-
+  入力した文字列の QR コードをテキストで表示する
   """
-  def hello do
-    :world
+  def show(string) when is_binary(string) do
+    string
+    |> QRCode.create()
+    ~> show_matrix()
+  end
+
+  defp show_matrix(%{matrix: matrix}) do
+    len = length(hd(matrix))
+
+    edge = [@white, String.duplicate("  ", len + 2), @black]
+
+    IO.puts(edge)
+
+    matrix
+    |> Enum.each(fn row ->
+      IO.write([@white, "  ", @black])
+
+      row
+      |> Enum.map(fn
+        1 -> [@black, "  "]
+        0 -> [@white, "  "]
+      end)
+      |> IO.write()
+
+      IO.puts([@white, "  ", @black])
+    end)
+
+    IO.puts(edge)
   end
 end
--- test/qr_test.exs
+++ test/qr_test.exs
@@ -1,8 +1,4 @@
 defmodule QrTest do
   use ExUnit.Case
   doctest Qr
-
-  test "greets the world" do
-    assert Qr.hello() == :world
-  end
 end
